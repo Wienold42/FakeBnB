117f9a8b6484b7c17cf36370650b7f4077fd13df
Wienold42 <jbrendanwienold@gmail.com>
2024-12-11 20:43:09 -0500

    first initial push w/ Marlon

---
+++ backend/routes/index.js
@@ -0,0 +1,20 @@
+// backend/routes/index.js
+const express = require('express');
+const router = express.Router();
+const apiRouter = require('./api');
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken
+    });
+  });
+
+router.use('/api', apiRouter);
+
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+  });
+  
+module.exports = router;
